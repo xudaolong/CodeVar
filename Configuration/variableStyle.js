3d11113ee0d98377e980146642474d1047ef2f99
xudaolong <xudaolong@vip.qq.com>
2016-12-13 14:03:09 +0800

    更新代码风格

--- Configuration/variableStyle.js
+++ Configuration/variableStyle.js
@@ -11,9 +11,53 @@
 
 'use strict';
 
+const alfy = require('alfy');
 const without = require('lodash.without');
 
 module.exports = {
+    core: function (api, params, style) {
+        alfy.fetch(api, params).then(result => {
+            if (result.errorCode == 0) {
+                //结果
+                let result_value = [];
+                // 过滤中文
+                let reg = /^[a-zA-Z ]/;
+                // 标准翻译结果 : translation
+                let result_translation = result.translation;
+                for (let i = 0, len = result_translation.length; i < len; i++) {
+                    if (reg.test(result_translation[i])) {
+                        result_value.push({
+                            title: style(result_translation[i]),
+                            subtitle: `标准翻译 => ${result_translation[i]}`,
+                            arg: style(result_translation[i]),
+                        });
+                    }
+                }
+                // 网络翻译 : web
+                if (result.web) {
+                    let result_web = result.web;
+                    for (let i = 0, len = result_web.length; i < len; i++) {
+                        for (let j = 0, ilen = result_web[i].value.length; j < ilen; j++) {
+                            if (reg.test(result_web[i].value[j])) {
+                                result_value.push({
+                                    title: style(result_web[i].value[j]),
+                                    subtitle: `网络翻译 => ${result_web[i].value[j]}`,
+                                    arg: style(result_web[i].value[j]),
+                                });
+                            }
+                        }
+                    }
+                }
+                alfy.output(result_value);
+            } else {
+                alfy.output([{
+                    title: '抱歉',
+                    subtitle: `无相关记录`,
+                    arg: 'error',
+                }]);
+            }
+        });
+    },
     bigHump: function bigHump(s) {
         // 过滤冠词,有需要的自己添加咯
         let strArr = without(s.split(' '), 'the', 'The');
--- underline.js
+++ underline.js
@@ -11,51 +11,10 @@
 
 'use strict';
 
-const alfy = require('alfy');
 const without = require('lodash.without');
 const config = require('./Configuration/variableGlobale.js');
 const style = require('./Configuration/variableStyle.js');
 
-alfy.fetch(config.youDaoApi, config.params).then(result => {
-    if (result.errorCode == 0) {
-        //结果
-        let result_value = [];
-        // 过滤中文
-        let reg = /^[a-zA-Z ]/;
-        // 标准翻译结果 : translation
-        let result_translation = result.translation;
-        for (let i = 0, len = result_translation.length; i < len; i++) {
-            if (reg.test(result_translation[i])) {
-                result_value.push({
-                    title: style.underline(result_translation[i]),
-                    subtitle: `标准翻译 => ${result_translation[i]}`,
-                    arg: style.underline(result_translation[i]),
-                });
-            }
-        }
-
-        // 网络翻译 : web
-        if (result.web) {
-            let result_web = result.web;
-            for (let i = 0, len = result_web.length; i < len; i++) {
-                for (let j = 0, ilen = result_web[i].value.length; j < ilen; j++) {
-                    if (reg.test(result_web[i].value[j])) {
-                        result_value.push({
-                            title: style.underline(result_web[i].value[j]),
-                            subtitle: `网络翻译 => ${result_web[i].value[j]}`,
-                            arg: style.underline(result_web[i].value[j]),
-                        });
-                    }
-                }
-            }
-        }
-        alfy.output(result_value);
-    } else {
-        alfy.output([{
-            title: '抱歉',
-            subtitle: `无相关记录`,
-            arg: 'error',
-        }]);
-    }
-})
-;
+(function () {
+    style.core(config.youDaoApi,config.params,style.underline);
+})();
